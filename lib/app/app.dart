a661a53021349881be42306c7a0959fe2120d1ee
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-07-11 22:22:58 +0200

    adding app folder

---
+++ lib/app/app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Cartella extends StatelessWidget {
+  const Cartella({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
