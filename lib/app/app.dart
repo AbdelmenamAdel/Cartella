c7739e741532944e17986a032507893158e49158
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-07-12 08:31:56 +0200

    add packages to app and handle system analysis

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -7,7 +7,9 @@ class Cartella extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        
+      ),
     );
   }
 }
